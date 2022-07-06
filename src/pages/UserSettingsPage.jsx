2f2307ddf7f52fa5059c5a0ed19c587f3dcaec4d
Pierre Carette <pierre.carette78@gmail.com>
2022-07-06 14:19:42 +0200

    nothing is working anymore

--- src/pages/UserSettingsPage.jsx
+++ src/pages/UserSettingsPage.jsx
@@ -10,6 +10,7 @@ const API_URL = "http://localhost:5005";
 function UserSettings() {
   const storedToken = localStorage.getItem("authToken");
   const [userInfo, setUserInfo] = useState({});
+
   const { isLoggedIn, isLoading, user } = useContext(AuthContext);
   useEffect(() => {
     (
@@ -21,6 +22,8 @@ function UserSettings() {
           },
         })
         .then((response) => {
+          console.log("RES", response.data);
+          if (!response.data.address) response.data.address = {city:'', country:'', number:'', zipcode:'', street:''}
           setUserInfo(response.data);
           // console.log(response.data);
           return userInfo;
@@ -33,20 +36,40 @@ function UserSettings() {
   }, []);
   const handleSubmit = (e) => {
     e.preventDefault();
+    const fd = new FormData();
     console.log(e.target);
-    const { firstName, lastName, address } = userInfo;
+    const { firstName, lastName, zipcode, street, number, city, country, imageUrl } = userInfo;
+    fd.append("firstName", firstName);
+    fd.append("lastName", lastName);
+    fd.append("imageUrl", imageUrl);
+    fd.append("zipcode", zipcode);
+    fd.append("street", street);
+    fd.append("number", number);
+    fd.append("city", city);
+    fd.append("country", country);
+
+    for (let pair of fd.entries()) {
+      console.log(pair[0], pair[1])
+    }
+
     const storedToken = localStorage.getItem("authToken");
-    axios.put(
-      `${API_URL}/user`,
-      { firstName, lastName, address },
-      {
+    axios
+      .put(`${API_URL}/user`, fd, {
         headers: {
           Authorization: `Bearer ${storedToken}`,
         },
-      }
-    );
+      })
+      .then((res) => {
+        if (res.status == 200) {
+          console.log("chien");
+          document.getElementsByClassName("submission").innerHTML = (
+            <p>Successfully modified</p>
+          );
+        }
+      });
   };
 
+  if (!Object.keys(userInfo).length) return <div>Loading</div>;
   return (
     <>
       <h2>User informations :</h2>
@@ -56,7 +79,7 @@ function UserSettings() {
       <p>
         You can add of modify certain informations here. Here are the fields :
       </p>
-      <form onSubmit={handleSubmit}>
+      <form className="submission" onSubmit={handleSubmit}>
         <label>
           First Name :
           <input
@@ -73,7 +96,7 @@ function UserSettings() {
           <input
             type="text"
             name="lastName"
-            value={userInfo.lastName || ""}
+            value={userInfo.lastName}
             onChange={(e) =>
               setUserInfo({ ...userInfo, [e.target.name]: e.target.value })
             }
@@ -82,84 +105,58 @@ function UserSettings() {
         <p>Address :</p>
         <label>
           Number :
-          <input type="number" value={userInfo.address?.number || ""}
-            onChange={(e) => {
-              const copyOfUserInfo = { ...userInfo };
-              copyOfUserInfo.address.number = e.target.value;
-              setUserInfo(copyOfUserInfo);
-            }}
-           />
+          <input
+            type="number"
+            value={userInfo.address?.number}
+            onChange={(e) => setUserInfo({ ...userInfo, [e.target.name]: e.target.value })}
+          />
         </label>
         <label>
           street :
           <input
             type="text"
-            value={
-              ("address" in userInfo &&
-                "street" in userInfo.address &&
-                userInfo.address.street) ||
-              ""
-            }
-            onChange={(e) => {
-              const copyOfUserInfo = { ...userInfo };
-              copyOfUserInfo.address.street = e.target.value;
-              setUserInfo(copyOfUserInfo);
-            }}
+            value={userInfo.address?.street}
+            onChange={(e) => setUserInfo({ ...userInfo, [e.target.name]: e.target.value })}
           />
         </label>
         <label>
           zipcode :
           <input
             type="text"
-            value={
-              ("address" in userInfo &&
-                "zipcode" in userInfo.address &&
-                userInfo.address.zipcode) ||
-              ""
-            }
-            onChange={(e) => {
-              const copyOfUserInfo = { ...userInfo };
-              copyOfUserInfo.address.zipcode = e.target.value;
-              setUserInfo(copyOfUserInfo);
-            }}
+            value={userInfo.address?.zipcode}
+            onChange={(e) => setUserInfo({ ...userInfo, [e.target.name]: e.target.value })}
           />
         </label>
         <label>
           city :
           <input
             type="text"
-            value={
-              ("address" in userInfo &&
-                "city" in userInfo.address &&
-                userInfo.address.city) ||
-              ""
-            }
-            onChange={(e) => {
-              const copyOfUserInfo = { ...userInfo };
-              copyOfUserInfo.address.city = e.target.value;
-              setUserInfo(copyOfUserInfo);
-            }}
+            value={userInfo.address?.city}
+            onChange={(e) => setUserInfo({ ...userInfo, [e.target.name]: e.target.value })}
           />
         </label>
         <label>
           country :
           <input
             type="text"
-            value={
-              ("address" in userInfo &&
-                "country" in userInfo.address &&
-                userInfo.address.country) ||
-              ""
-            }
-            onChange={(e) => {
-              const copyOfUserInfo = { ...userInfo };
-              copyOfUserInfo.address.country = e.target.value;
-              setUserInfo(copyOfUserInfo);
-            }}
+            value={userInfo.address?.country}
+            onChange={(e) => setUserInfo({ ...userInfo, [e.target.name]: e.target.value })}
           />
         </label>
 
-        <Button type="submit" variant="outlined">Apply changes</Button>
+        <input
+          type="file"
+          name="avatar-image"
+          onChange={(e) => {
+            const copyOfUserInfo = { ...userInfo };
+            copyOfUserInfo.imageUrl = e.target.files[0];
+            setUserInfo(copyOfUserInfo);
+          }}
+        />
+
+        <Button type="submit" variant="outlined">
+          Apply changes
+        </Button>
       </form>
     </>
   );
