bbb2ec32bd4986868cb882edadcb9b13ea56d583
RomainC75 <rom.chenard@gmail.com>
2022-07-07 10:48:52 +0200

    ..

--- src/components/CarouselImageCounter.jsx
+++ src/components/CarouselImageCounter.jsx
@@ -2,25 +2,37 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle, faCircleDot } from "@fortawesome/free-solid-svg-icons";
 
-import './styles/CarouselImageCounter.css'
+import "./styles/CarouselImageCounter.css";
 
 export default function CarouselImageCounter({ index, maxIndex }) {
   const [arrayOfIndexes, setArrayOfIndexes] = useState([]);
 
   useEffect(() => {
-    const array=[]
-    for (let i=0 ; i < maxIndex; i++) {
+    const array = [];
+    for (let i = 0; i < maxIndex; i++) {
       array.push(i <= index ? true : false);
     }
-    setArrayOfIndexes(array)
-    console.log('array',index,maxIndex,array)
+    setArrayOfIndexes(array);
+    console.log("array", index, maxIndex, array);
   }, [index, maxIndex]);
 
-  return( 
-  <div className="CarouselImageCounter index">
-    
-    {arrayOfIndexes.map((val,i)=>{
-        return (val?<FontAwesomeIcon key={`${val}-${i}`} className="carouselImageCounter blue icon" icon={faCircle}/>:<FontAwesomeIcon key={val} className="carouselImageCounter blue" icon={faCircleDot}/>)
-    })}
-  </div>);
+  return (
+    <div className="CarouselImageCounter index">
+      {arrayOfIndexes.map((val, i) => {
+        return val ? (
+          <FontAwesomeIcon
+            key={`${val}-${i}`}
+            className="carouselImageCounter blue icon"
+            icon={faCircle}
+          />
+        ) : (
+          <FontAwesomeIcon
+            key={`${val}-${i}`}
+            className="carouselImageCounter blue"
+            icon={faCircleDot}
+          />
+        );
+      })}
+    </div>
+  );
 }
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,24 +3,24 @@ import {useEffect, useState} from 'react'
 import React from 'react'
 import axios from 'axios'
 import './styles/Review.css'
+import { useParams } from 'react-router-dom'
 
 const API_URL = "http://localhost:5005";
 
-
-
-export default function Reviews({productId}) {
-    
+export default function Reviews() {
+    const {id} = useParams()
     const [ reviewsState, setReviewsState ] = useState({})
     const [ isLoading,  setIsLoading ]=useState(false)
     const [ isLoaded, setIsLoaded ] = useState(false)
     const [ isError, setIsError ] = useState(false)
     const [ currentPage, setCurrentPage ] = useState(1)
     const [ errorMessage, setErrorMessage ] = useState(false)
-    console.log("productId",productId)
+    console.log("id",id)
 
     const updateReviews = (page) =>{
+        console.log(('id', id))
         setIsLoading(true)
-        axios.get(`${API_URL}/review/${productId}?page=${page}`)
+        axios.get(`${API_URL}/review/${id}?page=${page}`)
         .then(res=>{
             
             setReviewsState(res.data)
@@ -48,13 +48,11 @@ export default function Reviews({productId}) {
     }
 
 
-
   return (
     <div className="Reviews">
         {isLoading && <div className="progress"></div>}
         {isLoaded && <div>
             <div className="reviewMenu">
-                
                 <button className="changeReviewsPage" onClick={()=>changePage(-1)}>-</button>
                 <p>{reviewsState.pagesInfo.page}/{reviewsState.pagesInfo.totalPageNumber}</p>
                 <button className="changeReviewsPage" onClick={()=>changePage(1)}>+</button>
