4528f0fa61a8cf7026d1dba7036623ddb9b8dab1
RomainC75 <rom.chenard@gmail.com>
2022-07-07 11:46:47 +0200

    offlineCart + quantity visible in cart page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -65,6 +65,7 @@ export default function Cart() {
           <li key={`cart-${product._id}-${i}`}>
             <h3>{product.productId.name}</h3>
             <img src={product.productId.pictures[0]} alt={`${product.productId.name} ${i}`}/>
+            <p>Quantity : <span>{product.quantity}</span></p>
             <div className="toRemove" onClick={()=>removeItemInDownloadedCart(product.productId._id)}>Remove</div>
           </li>);
       })}
